076df7859a9e8806b1f9a2161961978fb8c0082d
Jesper Sonne <jesper.sonne@sund.ku.dk>
2024-12-18 23:16:37 +0100

    Update AvianRangeShapes.jl

--- src/AvianRangeShapes.jl
+++ src/AvianRangeShapes.jl
@@ -3,5 +3,5 @@ module AvianRangeShapes
 using ArchGDAL,Rasters, DataFrames,Plots, SpreadingDye, NearestNeighbors, Images, JLD2, SkipNan, 
 include("functions.jl")
 
-export null_models
+export AvianRangeShapes
 end
