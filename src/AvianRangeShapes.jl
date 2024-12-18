f1c43140621d523248fa611a78b5829bc074ca5d
Jesper Sonne <jesper.sonne@sund.ku.dk>
2024-12-18 23:22:39 +0100

    Update AvianRangeShapes.jl

--- src/AvianRangeShapes.jl
+++ src/AvianRangeShapes.jl
@@ -1,4 +1,5 @@
 module AvianRangeShapes
+Pkg.add(url="https://github.com/mkborregaard/SpreadingDye.jl")
 
 using ArchGDAL,Rasters, DataFrames,Plots, SpreadingDye, NearestNeighbors, Images, JLD2, SkipNan
 include("functions.jl")
